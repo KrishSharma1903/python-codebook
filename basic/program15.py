df851397368ffbdb78062cf0859779589e8ad8d3
krishSharma1903 <KrishSharma1903@gmail.com>
2026-06-29 00:11:55 +0530

    commit

---
+++ basic/program15.py
@@ -0,0 +1,10 @@
+number =  int(input("Enter a number: "))
+
+if number==0:
+    print("The number is zero")
+
+elif number>0: 
+    print ("The number is postive")
+
+else: 
+    print ("The number is negative")
