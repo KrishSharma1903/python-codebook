5383f658de01b7f9dc82bd7f8d64b359658e43f2
krishSharma1903 <KrishSharma1903@gmail.com>
2026-06-29 22:17:31 +0530

    commit

---
+++ python-programs/02 Conditional/postive_negative4.py
@@ -0,0 +1,11 @@
+number =  int(input("Enter a number: "))
+
+if number>0: 
+    if number % 2 == 0:
+        print("Number is Positive and even")
+    else:
+          print("Number is Positive and odd")
+
+
+else: 
+    print ("The number is negative")
