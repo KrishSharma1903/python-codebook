bbd26cd7e445f04b7d5ff36ef2464608832c2833
krishSharma1903 <KrishSharma1903@gmail.com>
2026-06-29 22:17:46 +0530

    commit

---
+++ python-programs/02 Conditional/rollercoaster2.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in cm? ")) 
+age = int(input("Enter your age: "))
+
+if height > 120:
+    print("You can ride the roller coaster!!")
+    
+    if age<=12:
+        print("Your ticket price is 700")
+    elif age<=18:
+        print("Your ticket price is 1200 ")
+    else:
+        print("Your ticket price is 1600 ")
+
+else:
+    print("You need to grow up before you can ride")
