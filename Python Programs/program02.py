e7c8814d8e0787bd7e5d087582cfb1b334bba92a
krishSharma1903 <KrishSharma1903@gmail.com>
2026-06-29 21:23:09 +0530

    Remove duplicate folder and accidental git file

--- Python Programs/program02.py
+++
@@ -1,4 +0,0 @@
-username =  input("Enter your name: ")
-length =  len(username)
-
-print(f"length of the name is {length} " )
--- Python Programs/program05.py
+++
@@ -1,4 +0,0 @@
-name = input("Emter your name: ")
-length  = len(name)
-
-print("Your name is "+ name +" " + "Number of letter in your name is " +str(length)) 
