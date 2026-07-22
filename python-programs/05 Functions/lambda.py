1e3bd6025d4e1debccf6cad7831cea6f43c70da1
krishSharma1903 <KrishSharma1903@gmail.com>
2026-07-22 21:21:39 +0530

    commit

---
+++ python-programs/05 Functions/lambda.py
@@ -0,0 +1,12 @@
+addition = lambda x ,y : x + y 
+
+# print(addition(12, 13))
+
+even = lambda num : num%2 == 0
+
+# print(even(23))
+
+
+addition1 = lambda x ,y ,z : x + y + z
+
+print(addition1(12,11,134))
