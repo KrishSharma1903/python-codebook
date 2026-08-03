d686fe5b0386264bd62dc196f0418856097d271c
krishSharma1903 <KrishSharma1903@gmail.com>
2026-08-03 16:14:06 +0530

    commit

---
+++ python-programs/01 Basics/basics.py
@@ -0,0 +1,74 @@
+# comments 
+
+## This is a single line comment 
+
+'''
+This is a multi line comment 
+'''
+## BASICS  SYNTAX OF PYTHON
+
+## Pyton is case sensative 
+
+name = "Krish"
+Name = "Sharma"
+print(name)
+print(Name)
+
+
+## Identation
+## Python uses Indentation to define blocks of code. Comsistent use of spaces(commonly 4) or a tab is required.
+
+age = 32
+
+if age > 30:
+    print(age)
+
+
+
+
+## Line Continuation
+total = 1+2+3+4+5+\
+4+6
+
+print(total)
+
+
+##Multiple Statemnts on a single line 
+
+x=5;y=10;z=x+y
+print(z)
+
+
+##UNDERSTANDING SEMANTICS IN PYTHON
+
+# variable assignment
+
+age =32 # type automatically detected by kernel 
+name = "Krish"
+
+type(name)
+
+
+#type Inference
+variable = 10
+print(type(variable))
+
+variable = "Krish"
+print(type(variable))
+
+
+## some basic errors
+
+# #indenration error 
+# age = 32
+
+# if age > 30:
+# print(age)
+
+# #name error
+
+# a=b
+
+
+
+
