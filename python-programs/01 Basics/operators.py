c2f27635c5c22aa956e6d85449320c902bf9f3e3
krishSharma1903 <KrishSharma1903@gmail.com>
2026-08-05 00:23:00 +0530

    commit

---
+++ python-programs/01 Basics/operators.py
@@ -0,0 +1,65 @@
+## Arithmatic Operator
+a = 10
+b = 5
+
+add = a + b # addition 
+sub = a - b # subtraction 
+mul = a * b # multiplication
+div = a / b # division 
+floor_div = a // b # floor division -> result as integer
+modulus = a % b # modulus -> gives remainder 
+expo =  a ** b # exponential -> a^b
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(floor_div)
+print(modulus)
+print(expo)
+
+
+##Comparision Operators 
+ #Equals to Operator(==) 
+a = 10
+b = 10
+print(a==b)
+
+str_1 = "Krish"
+str_2 = "KriSh"
+print(str_1== str_2)
+
+#Not equals to (!=)
+str_3 = "Krish"
+str_4 = "KriSh"
+print(str_3!= str_4)
+
+#Greater than > and lesser than <
+num1 = 45
+num2 = 55
+print(num1>num2)
+print(num1<num2)
+
+#Greater than and equals to => and lesser than and equals to <=
+number1 = 45 
+number2 = 45
+print(number1>=number2)
+
+##Logical Operator (AND , OR , NOT )
+#AND operator
+x = True
+y = True
+
+result = x and y
+print(result)
+
+#OR operator
+x = True
+y = False
+
+result = x or y
+print(result)
+
+#NOT operator
+x = True
+print(not x)
