a885e48b97a4c963c0ca1ed750305d44847d6889
krishSharma1903 <KrishSharma1903@gmail.com>
2026-08-05 00:22:27 +0530

    commit

---
+++ python-programs/01 Basics/data_types.py
@@ -0,0 +1,21 @@
+##Integer Example
+age = 21
+print(type(age))
+
+##Float Example
+pi = 3.142
+print(pi)
+print(type(pi))
+
+##String Example
+name = "Krish"
+print(name)
+print(type(name))
+
+##Boolean Example
+is_true = True
+print(type(is_true))
+
+a=10
+b=10
+print(type(a==b))
