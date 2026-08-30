84847c548bd002eb2c2f70ac2655248e8fbe70b5
krishSharma1903 <KrishSharma1903@gmail.com>
2026-08-31 01:01:29 +0530

    commit

---
+++ python-programs/08 Tuple_and_sets/counting_unique_words.py
@@ -0,0 +1,7 @@
+text = "Hello my name name is is Krish "
+words=text.split()
+print(words)
+#convert list of words to set to get unique words
+unique_words = set(words)
+print(unique_words)
+print(len(unique_words))
