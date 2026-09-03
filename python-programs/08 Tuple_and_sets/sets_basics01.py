3fc1ffc167c81c1c48a6efcc91a35b7421ae14a9
krishSharma1903 <KrishSharma1903@gmail.com>
2026-09-03 14:37:10 +0530

    commit

---
+++ python-programs/08 Tuple_and_sets/sets_basics01.py
@@ -0,0 +1,16 @@
+##Creating a set 
+my_set = {1,2,3,4}
+print(my_set)
+print(type(my_set))
+
+#Creating an empty set
+my_empty_set = set()
+print(type(my_empty_set))
+
+#typecasting to set
+my_set = set([1,2,3,4,5])
+print(my_set)
+
+#Set can only have unique elements
+my_set = set([1,1,2,2,3,5,4,5])
+print(my_set)
