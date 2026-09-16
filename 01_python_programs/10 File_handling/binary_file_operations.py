5b75c425045ebf2979e54cf7be4665b6d37cf833
krishSharma1903 <KrishSharma1903@gmail.com>
2026-09-17 04:04:00 +0530

    commit

---
+++ 01_python_programs/10 File_handling/binary_file_operations.py
@@ -0,0 +1,13 @@
+## Binary files
+
+# Writing to a binary file
+data = b'\x00\x01\x02\x03\x04'
+with open('example.bin', 'wb') as file:
+    file.write(data)
+
+# Reading a binary file
+with open('example.bin', 'rb') as file:
+    content = file.read()
+    print(content)
+
+
